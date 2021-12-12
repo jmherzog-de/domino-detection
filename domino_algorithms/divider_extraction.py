7287045f715583f6994ab0eab99319435aa58737
Jean-Marcel Herzog <dev@jmherzog.de>
2021-12-12 17:34:06 +0100

    Update divider_extraction.py

--- domino_algorithms/divider_extraction.py
+++ domino_algorithms/divider_extraction.py
@@ -24,10 +24,11 @@ from domino_algorithms.domino_stone import DominoStone
 class DividerExtraction:
 
     @staticmethod
-    def ExtractDividers(cvImage: np.ndarray, minArea: int, cvOutImage: np.ndarray):
+    def ExtractDividers(cvImage: np.ndarray, minArea: int, cvOutImage: np.ndarray, dominos_list: list):
         
         
         domino_stones = list()
+        dominos_list.clear()
 
         #
         # Find Contours
@@ -49,16 +50,23 @@ class DividerExtraction:
 
             # get dimensions
             (center), (width, height), angle = rot_rect
-
+            
             
             # get the center line from box
             # note points are clockwise from bottom right
             if width > height:
+
+                if 3 * height > width:
+                    continue
+
                 x1 = (box[0][0] + box[1][0]) // 2
                 y1 = (box[0][1] + box[1][1]) // 2
                 x2 = (box[3][0] + box[2][0]) // 2
                 y2 = (box[3][1] + box[2][1]) // 2
             else:
+
+                if 3 * width > height:
+                    continue
                 x1 = (box[0][0] + box[3][0]) // 2
                 y1 = (box[0][1] + box[3][1]) // 2
                 x2 = (box[1][0] + box[2][0]) // 2
@@ -68,7 +76,7 @@ class DividerExtraction:
             # compute center line length
             cl_length = math.sqrt( (x1-x2)**2 + (y1-y2)**2)
 
-            domino_stones.append(DominoStone(center_x=center[0], center_y=center[1], length=cl_length, width=width, height=height, angle_deg=angle))
+            dominos_list.append(DominoStone(center_x=center[0], center_y=center[1], length=cl_length, width=width, height=height, angle_deg=angle, center_line_p1_x=x1, center_line_p1_y=y1, center_line_p2_x=x2, center_line_p2_y=y2))
                                     
             cv2.drawContours(cvOutImage, [box], 0, (255, 255, 255), 1)
 
@@ -77,4 +85,4 @@ class DividerExtraction:
             cv2.line(cvOutImage, pt1=(x1, y1), pt2=(x2, y2), color=(0, 0, 255), thickness=2)
           
 
-        return domino_stones
+        return domino_stones.copy()
